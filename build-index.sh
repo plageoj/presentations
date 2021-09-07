b9ea8dd1a82c31be17d04d0830468c675ca914ff
Masayuki Sugahara <equaaqua@hotmail.com>
2021-09-07 21:49:43 +0900

    reordered index

--- build-index.sh
+++ build-index.sh
@@ -19,19 +19,14 @@ HTML
 
 IFS=$'\n'
 
-for line in `ls -tl --full-time *.md | sed -e "s/ \+/ /g"`; do
-  file=`echo $line | cut -f9 -d" " | sed "s/.md//g"`
+for file in `ls -r *.md | sed -e "s/.md//g"`; do
   title=`grep "title:" $file.md | cut -f2 -d":" | xargs`
 
   if [ "x$title" != "x" ]; then
-    date=`echo $line | cut -f6 -d" "`
-    echo -e "$file:\t$date\t[$title]"
+    echo -e "$file:\t[$title]"
     cat << HTML >> index.html
   <dt>
     <a href="./$file.html">$title</a>
-    <small>
-      最終更新: <time>$date</time>
-    </small>
   </dt>
   <dd>
     [<a href="./$file.md">markdown</a>]
